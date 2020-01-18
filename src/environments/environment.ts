41eceeae18054f260b767f0d9ba4ec8fdcc5192f
manjubagade <themanju.bagade@gmail.com>
2020-01-18 13:26:52 +0530

    change base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  baseUrl : 'https://fundoonotes.incubation.bridgelabz.com/api/',
+  baseUrl : 'http://fundoonotes.incubation.bridgelabz.com/api/',
 };
 
 /*
